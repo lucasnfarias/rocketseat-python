7340bf3173ea32a4505c88e1ad3b55339227fb22
Lucas Farias <lucas.farias@rtbhouse.com>
2024-05-04 21:46:36 -0300

    (module02/first-flask) create get and post tasks routes

--- module02/first-flask/app.py
+++ module02/first-flask/app.py
@@ -1,18 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  return "Hello world!"
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def about():
-  return "Sobre o site:"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(task_id_control, data['title'], data['description'])
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({ "message": "Nova tarefa criada com sucesso!" })
 
-@app.route("/tasks")
-def tasks():
-  return []
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  task_list = [task.to_dict() for task in tasks]
+
+  return jsonify({
+    "tasks": task_list,
+    "total_tasks": len(task_list)
+  })
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task_by_id(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      return jsonify(t.to_dict())
+
+  return jsonify({ "message": f"Atividade com id {id} não existe." }), 404
+
 
 if __name__ == "__main__":
   app.run(debug=True)
